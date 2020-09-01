5eda46be2bb345ebf8b25457693c2605816464e7
Jeferson De Oliveira Dos Santos <69129@PROWAY.TREINA>
2020-09-01 20:45:42 -0300

    nova estrutura

---
+++ script.js
@@ -0,0 +1,22 @@
+
+var x = 5;
+var y = 9;
+
+var somar = (x + y);
+var subtrair = (x - y);
+var dividir = (x / y);
+var multiplicar = (x *y);
+
+/*
+alert("soma é:" + somar);
+alert("subttracao é:" + subtrair);
+alert("multiplicacao é:" + multiplicar);
+alert("divisao é: "  + dividir);
+*/
+
+var ternario = somar >= 0 ? alert("é maior") : alert("é menor");
+
+function somando (a,b){
+    alert("o valor da soma é: " + a+b)
+}
+somando(1,3);
